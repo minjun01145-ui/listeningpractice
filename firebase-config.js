a0982bf44bc337e152630b092b019cd016e0061a
minjun01145-ui <minjun01145@gmail.com>
2026-08-26 15:20:09 +0900

    236236

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Firebase 콘솔 > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성의 값을 붙여넣으세요.
 // 이 파일의 Firebase API key는 웹 클라이언트 식별용이며, 서버 비밀번호처럼 취급하는 비밀키는 아닙니다.
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAh3e5ruxctlhv-OwBAQl5WDds0IZooPD0",
+  authDomain: "test2222-e2458.firebaseapp.com",
+  projectId: "test2222-e2458",
+  storageBucket: "test2222-e2458.firebasestorage.app",
+  messagingSenderId: "848561047931",
+  appId: "1:848561047931:web:17d0d6ed18b89136e195de",
+  measurementId: "G-DN2WFYXT9N"
 };
